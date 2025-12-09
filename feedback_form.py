2abff8d45d0bb44da6f06c3552da1c3522b3b95a
rose <rose@MacBook-Air.local>
2025-12-09 05:17:47 -0800

    check

--- feedback_form.py
+++ feedback_form.py
@@ -127,7 +127,7 @@ else:
             "Warehouse finder",
             "Quotation",
             "Quotation sharing with customer",
-            "Lead stages and lost reason"
+            "Lead stages and lost reason",
             "Log note",
             "Email notifications on lead actions",
             "Customer dashboard",
